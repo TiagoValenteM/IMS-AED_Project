124897d3f5d20d27d0085e181eaa39bfb7f326a3
Tiago Valente <tiagomilheiro13@outlook.pt>
2022-06-02 00:14:32 +0100

    V1.0.0

--- competing_sorting_algorithms/run_tests.py
+++ competing_sorting_algorithms/run_tests.py
@@ -4,10 +4,8 @@ from sorting_algorithms import bubble_sort, counting_sort, heap_sort, insertion_
 from user_inputs import user_input
 from random import randint
 
-
 def time(time_sec):
     return f'{time_sec:.5f} sec'
-
 
 # initial list input
 ini_input = input("What is the initial size of your list? ")
@@ -17,14 +15,14 @@ initial = user_input(ini_input)
 inc_input = input("What is the incremental size of your list? ")
 incremental = user_input(inc_input)
 
-# number of iterations
+# number of iterations input
 iti_input = int(input("How many iterations do you want on your list? "))
 
-# range of random numbers
+# range of random numbers input
 range_input = input("What is the range of random numbers in your list? ")
 range_numbers = user_input(range_input)
 
-
+# Lists creation for each row in matrix
 iterations_result = []
 algorithms_result = []
 bubble_result = []
@@ -38,22 +36,31 @@ selection_result = []
 
 for number in range(iti_input):
     iterations_result.append(number + 1)
+
     values_list = [randint(0, range_numbers) for i in range(initial + incremental * number)]
     algorithms_result.append(initial + incremental * number)
+
     bubble_sorted, bubble_time = bubble_sort.sort(values_list.copy())
     bubble_result.append(time(bubble_time))
+
     counting_sorted, counting_time = counting_sort.sort(values_list.copy())
     counting_result.append(time(counting_time))
+
     heap_sorted, heap_time = heap_sort.sort(values_list.copy())
     heap_result.append(time(heap_time))
+
     insertion_sorted, insertion_time = insertion_sort.sort(values_list.copy())
     insertion_result.append(time(insertion_time))
+
     merge_sorted, merge_time = merge_sort.sort(values_list.copy())
     merge_result.append(time(merge_time))
+
     quick_sorted, quick_time = quick_sort.sort(values_list.copy())
     quick_result.append(time(quick_time))
+
     radix_sorted, radix_time = radix_sort.sort(values_list.copy())
     radix_result.append(time(radix_time))
+
     selection_sorted, selection_time = selection_sort.sort(values_list.copy())
     selection_result.append(time(selection_time))
 
@@ -68,6 +75,7 @@ matrix = [["Iteration", iterations_result],
           ["Radix Sort", radix_result],
           ["Selection Sort", selection_result]]
 
+
 dash = '-' * ((iti_input+1) * 26)
 for line in matrix:
     print(f'\n{dash}')
@@ -78,4 +86,3 @@ for line in matrix:
         print(f'{str(secs):<25s}', end='')
 print()
 print()
-
--- competing_sorting_algorithms/user_inputs.py
+++ competing_sorting_algorithms/user_inputs.py
@@ -1,19 +1,3 @@
-from random import randint
-
-
-def iteration_add(ini, inc, ite):
-    new_el_count = inc * ite
-
-    for i in range(new_el_count):
-        if i == 0:
-            ini = ini
-        else:
-            new_element = ite * inc
-            for number in new_element:
-                ini.append(number)
-    return ini
-
-
 def user_input(ini_size):
     ini_size = str(ini_size)
     ini_size = ini_size.upper()
@@ -26,17 +10,3 @@ def user_input(ini_size):
             ini_size = ini_size.replace("B", "000000000")
     ini_size = int(ini_size)
     return ini_size
-
-
-def list_iti(inc_size):
-    inc_size = str(inc_size)
-    inc_size = inc_size.upper()
-    for i in inc_size:
-        if i == "K":
-            inc_size = inc_size.replace("K", "000")
-        elif i == "M":
-            inc_size = inc_size.replace("M", "000000")
-        elif i == "B":
-            inc_size = inc_size.replace("B", "000000000")
-    inc_size = int(inc_size)
-    return inc_size
